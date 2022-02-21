cc29a177cb17562250a85ad06aadf124c4bfde6c
Jovan Filipovic <jovanfilipovicsport@gmail.com>
2022-02-21 16:04:20 +0100

    LocationPopUpPage, tested

--- src/pages/BasicPage.java
+++ src/pages/BasicPage.java
@@ -1,7 +1,16 @@
 package pages;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public abstract class BasicPage {
 	
+	WebDriver driver;
+	JavascriptExecutor js;
 	
+	public BasicPage(WebDriver driver) {
+		this.driver = driver;
+		js = (JavascriptExecutor) driver;
+	}
 }
--- src/tests/BasicTest.java
+++ src/tests/BasicTest.java
@@ -1,5 +1,62 @@
 package tests;
 
+import static org.testng.Assert.assertEquals;
+
+import java.io.File;
+import java.time.Duration;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import pages.LocationPopupPage;
+
 public class BasicTest {
+
+	WebDriver driver;
+	Actions action;
+	LocationPopupPage locPP;
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.setProperty("webdriver.chrome.driver", "driver-lib/chromedriver.exe");
+
+		driver = new ChromeDriver();
+		action = new Actions(driver);
+		locPP = new LocationPopupPage(driver);
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().window().maximize();
+		
+		driver.get("http://demo.yo-meals.com/");
+
+	}
+
+	@Test(enabled = true)
+	public void searchTableTest() throws Exception {
+
+		   locPP.closeJumpDialog();
+		   locPP.openLocationHeader();
+		   locPP.setLocation("Helderberg - Albany");
+		
+	}
+
+
+	@AfterMethod
+	public void afterMethod() throws InterruptedException {
+		Thread.sleep(15000);
+		driver.quit();
+	}
 
 }
